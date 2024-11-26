7420a8ca7d1177ff37cfef76e30b6496ba9ce999
mose31 <monamose15@gmail.com>
2024-11-26 19:06:08 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,5 +8,5 @@ t = st.number_input("Masukan Tinggi(cm): ",0)
 
 if st.button("Hitung volume",type="primary"):
   V = math.pi*(r**2)*t
-  st.success(f'Volume tapbung adalah {v:.2f}')
+  st.success(f'Volume tabung adalah {v:.2f}')
 
